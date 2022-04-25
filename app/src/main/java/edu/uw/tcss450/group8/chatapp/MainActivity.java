3a68819da543d4feeefe8772619752199507f22c
Shilnara Dam <shilnarawdam@gmail.com>
2022-04-24 17:40:59 -0700

    bottom navigation shown.

--- app/src/main/java/edu/uw/tcss450/group8/chatapp/MainActivity.java
+++ app/src/main/java/edu/uw/tcss450/group8/chatapp/MainActivity.java
@@ -2,14 +2,29 @@ package edu.uw.tcss450.group8.chatapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import androidx.navigation.ui.AppBarConfiguration;
+
+
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
+
+    private AppBarConfiguration mAppBarConfiguration;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         //test comment
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home)
+                .build();
     }
 }

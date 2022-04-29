7563edc815e1a331a8aa3d0914441206d5f67965
Shilnara Dam <shilnarawdam@gmail.com>
2022-04-28 19:48:29 -0700

    added home and setting fragments.

---
+++ app/src/main/java/edu/uw/tcss450/group8/chatapp/ui/home/HomeFragment.java
@@ -0,0 +1,53 @@
+package edu.uw.tcss450.group8.chatapp.ui.home;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import edu.uw.tcss450.group8.chatapp.R;
+import edu.uw.tcss450.group8.chatapp.databinding.FragmentHomeBinding;
+
+
+/**
+ * create an instance of this fragment.
+ */
+public class HomeFragment extends Fragment {
+
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        //Local access to the ViewBinding object. No need to create as Instance Var as it is only
+        //used here.
+        FragmentHomeBinding binding = FragmentHomeBinding.bind(getView());
+
+        //on button click,navigate to settings
+        binding.buttonSettings.setOnClickListener(button ->
+                Navigation.findNavController(getView()).navigate(
+                        HomeFragmentDirections
+                                .actionHomeFragmentToSettingFragment()));
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+    }
+}

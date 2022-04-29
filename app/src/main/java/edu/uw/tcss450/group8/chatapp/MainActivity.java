e275c3c36bc46db8be219627ac7eee77d411813c
Shilnara Dam <shilnarawdam@gmail.com>
2022-04-28 23:44:40 -0700

    added home and setting fragments with rebased main.

--- app/src/main/java/edu/uw/tcss450/group8/chatapp/MainActivity.java
+++ app/src/main/java/edu/uw/tcss450/group8/chatapp/MainActivity.java
@@ -3,12 +3,15 @@ package edu.uw.tcss450.group8.chatapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 
 import android.os.Bundle;
 
-import com.auth0.android.jwt.JWT;
+//import com.auth0.android.jwt.JWT;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import edu.uw.tcss450.group8.chatapp.model.UserInfoViewModel;
@@ -23,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
         //test comment
         super.onCreate(savedInstanceState);
 
+        setContentView(R.layout.activity_main);
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
@@ -30,9 +34,17 @@ public class MainActivity extends AppCompatActivity {
         mAppBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_home)
                 .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navView, navController);
 
+    }
 
-
-        setContentView(R.layout.activity_main);
+    //back button for home
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+                || super.onSupportNavigateUp();
     }
 }

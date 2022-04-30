4b0201430c5cabdf3f49d397aba690932d5cf002
Shilnara Dam <shilnarawdam@gmail.com>
2022-04-30 00:24:52 -0700

    added weather fragment and fix bottom nav bar.

--- app/src/main/java/edu/uw/tcss450/group8/chatapp/MainActivity.java
+++ app/src/main/java/edu/uw/tcss450/group8/chatapp/MainActivity.java
@@ -32,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home)
+                R.id.homeFragment, R.id.connectionsFragment, R.id.chatFragment, R.id.weatherFragment)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);

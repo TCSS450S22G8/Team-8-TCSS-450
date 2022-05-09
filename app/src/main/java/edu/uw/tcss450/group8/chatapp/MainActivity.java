d94c4db0fcfad9f6a2fa751de7358c2e6a247b24
Shilnara Dam <shilnarawdam@gmail.com>
2022-05-09 15:06:01 -0700

    Changed settings fragment to be navigated from top toolbar.

--- app/src/main/java/edu/uw/tcss450/group8/chatapp/MainActivity.java
+++ app/src/main/java/edu/uw/tcss450/group8/chatapp/MainActivity.java
@@ -9,9 +9,11 @@ import androidx.navigation.ui.NavigationUI;
 
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
-//import com.auth0.android.jwt.JWT;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 
 /**
  * Class for Main Activity
@@ -20,12 +22,11 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
  *
  * @author Charles Bryan
  * @author Shilnara Dam
- * @version 1.0
+ * @version 2.0
  */
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,7 +43,6 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
-
     }
 
     //back button for home
@@ -51,5 +51,25 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
+    }
+
+    //onCreateOptionsMenu and onOptionsItemSelected are top right, 3 dot settings
+    //toolbar.xml file
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        //gets id for item selected, then sends user to corresponding fragment.
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.settingFragment:
+                Navigation.findNavController(this, R.id.nav_host_fragment).navigate(R.id.nav_setting_fragment);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }

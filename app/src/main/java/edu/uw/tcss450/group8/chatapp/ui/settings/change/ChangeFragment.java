472534a840488204b1be4be22ea63a16c6e325c8
Levi McCoy (lmc56) <lmc56@uw.edu>
2022-05-11 03:54:31 -0700

    added change and forgot password

---
+++ app/src/main/java/edu/uw/tcss450/group8/chatapp/ui/settings/change/ChangeFragment.java
@@ -0,0 +1,220 @@
+package edu.uw.tcss450.group8.chatapp.ui.settings.change;
+
+import static edu.uw.tcss450.group8.chatapp.utils.PasswordValidator.checkClientPredicate;
+import static edu.uw.tcss450.group8.chatapp.utils.PasswordValidator.checkExcludeWhiteSpace;
+import static edu.uw.tcss450.group8.chatapp.utils.PasswordValidator.checkPwdDigit;
+import static edu.uw.tcss450.group8.chatapp.utils.PasswordValidator.checkPwdLength;
+import static edu.uw.tcss450.group8.chatapp.utils.PasswordValidator.checkPwdLowerCase;
+import static edu.uw.tcss450.group8.chatapp.utils.PasswordValidator.checkPwdSpecialChar;
+import static edu.uw.tcss450.group8.chatapp.utils.PasswordValidator.checkPwdUpperCase;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.Navigation;
+
+import edu.uw.tcss450.group8.chatapp.databinding.FragmentChangeBinding;
+import edu.uw.tcss450.group8.chatapp.utils.PasswordValidator;
+
+/**
+ * Class for the Register Fragment that handles user registration to the application.
+ * Adapted from original code by Charles Bryan.
+ *
+ * @author Charles Bryan
+ * @author Levi McCoy
+ * @version 1.0
+ */
+public class ChangeFragment extends Fragment {
+
+    private FragmentChangeBinding mBinding;
+
+    private ChangeViewModel mRegisterModel;
+
+
+
+    private PasswordValidator mEmailValidator = checkPwdLength(2)
+            .and(checkExcludeWhiteSpace())
+            .and(checkPwdSpecialChar("@"));
+
+    private PasswordValidator mPassWordValidator =
+            checkClientPredicate(pwd -> pwd.equals(mBinding.editChangePassword2.getText().toString()))
+                    .and(checkPwdLength(7))
+                    .and(checkPwdSpecialChar())
+                    .and(checkExcludeWhiteSpace())
+                    .and(checkPwdDigit())
+                    .and(checkPwdLowerCase().or(checkPwdUpperCase()));
+
+
+    /**
+     * Required empty constructor for the register fragment
+     */
+    public ChangeFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mRegisterModel = new ViewModelProvider(getActivity())
+                .get(ChangeViewModel.class);
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        mBinding = FragmentChangeBinding.inflate(inflater);
+        return mBinding.getRoot();
+    }
+
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+        mBinding.buttonChange.setOnClickListener(button -> {
+            attemptChange(button);
+            //Navigation.findNavController(getView()).navigate(
+            //RegisterFragmentDirections.actionRegisterFragmentToVerifyFragment());
+        });
+        /*
+        mRegisterModel.addResponseObserver(getViewLifecycleOwner(),
+                this::observeResponse
+        );
+
+         */
+
+
+    }
+
+    /**
+     * Attempts to Chane to the new password, validates the user entered information,
+     * then sends it to the server for validation.
+     *
+     * @param button button clicked
+     */
+    private void attemptChange(final View button) {
+        mBinding.layoutWait.setVisibility(View.VISIBLE);
+        validatePasswordsMatch();
+    }
+
+
+    /**
+     * Checks if the user input matches the required parameters for matching passwords.
+     * Then calls validate password.
+     */
+    private void validatePasswordsMatch() {
+        PasswordValidator matchValidator =
+                checkClientPredicate(
+                        pwd -> pwd.equals(mBinding.editChangePassword2.getText().toString().trim()));
+
+        mEmailValidator.processResult(
+                matchValidator.apply(mBinding.editChangePassword1.getText().toString().trim()),
+                this::validatePassword,
+                result -> {
+                    mBinding.editChangePassword2.setError("Passwords must match.");
+                    mBinding.layoutWait.setVisibility(View.GONE);
+                });
+    }
+
+    /**
+     * Checks if the user input matches the required parameters for passwords.
+     * Then calls cahck for old password.
+     */
+    private void validatePassword() {
+        mPassWordValidator.processResult(
+                mPassWordValidator.apply(mBinding.editChangePassword1.getText().toString()),
+                this::validatePassword2,
+                result -> {
+                    mBinding.editChangePassword1.setError("Please enter a valid Password.");
+                    mBinding.layoutWait.setVisibility(View.GONE);
+                });
+    }
+
+    /**
+     * Checks if the user input matches the required parameters for passwords.
+     * Then calls verify auth with server.
+     */
+    private void validatePassword2() {
+        mPassWordValidator.processResult(
+                mPassWordValidator.apply(mBinding.editChangeCurPass.getText().toString()),
+                this::verifyAuthWithServer,
+                result -> {
+                    mBinding.editChangeCurPass.setError("Please enter a valid Password.");
+                    mBinding.layoutWait.setVisibility(View.GONE);
+                });
+    }
+
+    /**
+     * Sends Asynchronous JSON request to the server for user registration
+     * information validation.
+     */
+    private void verifyAuthWithServer() {
+        navigateToLogin();
+        //mRegisterModel.connect(
+                //mBinding.editChangeCurPass.getText().toString(),
+                //mBinding.editChangePassword1.getText().toString());
+
+        //This is an Asynchronous call. No statements after should rely on the
+        //result of connect().
+
+
+    }
+
+
+    /**
+     * Navigates to the verify fragment to continue registration by verifying email.
+     */
+    private void navigateToLogin() {
+        // ToDO: Register to Verification to autofill login
+//        RegisterFragmentDirections.ActionRegisterFragmentToLoginFragment directions =
+//                RegisterFragmentDirections.actionRegisterFragmentToLoginFragment();
+//
+//        directions.setEmail(binding.editEmail.getText().toString());
+//        directions.setPassword(binding.editPassword1.getText().toString());
+
+        Navigation.findNavController(getView()).navigate(ChangeFragmentDirections.actionChangeFragmentToNavHomeFragment());
+
+    }
+
+
+    /**
+     * An observer on the HTTP Response from the web server. This observer should be
+     * attached to SignInViewModel.
+     *
+     * @param response the Response from the server
+     */
+    /*
+    private void observeResponse(final JSONObject response) {
+        if (response.length() > 0) {
+            if (response.has("code")) {
+                try {
+                    mBinding.editChangePassword1.setError(
+                            "Error Authenticating: " +
+                                    response.getJSONObject("data").getString("message"));
+                    mBinding.layoutWait.setVisibility(View.GONE);
+                } catch (JSONException e) {
+                    Log.e("JSON Parse Error", e.getMessage());
+                    mBinding.layoutWait.setVisibility(View.GONE);
+                }
+            } else {
+                navigateToLogin();
+            }
+        } else {
+            Log.d("JSON Response", "No Response");
+            mBinding.layoutWait.setVisibility(View.GONE);
+        }
+
+    }
+
+     */
+
+
+}

f019706f20098b8f8c5191477d387ccab2d001c6
slogansUW <slogans@uw.edu>
2022-05-18 17:16:42 -0700

    message functionality, without pushy, bug with initially populating sent message at the top of messages in the chat, after closing and re-opening that app the message is at the bottom

--- app/src/main/java/edu/uw/tcss450/group8/chatapp/ui/comms/chat/MessageListFragment.java
+++ app/src/main/java/edu/uw/tcss450/group8/chatapp/ui/comms/chat/MessageListFragment.java
@@ -1,34 +1,50 @@
 package edu.uw.tcss450.group8.chatapp.ui.comms.chat;
 
+
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
-
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import androidx.recyclerview.widget.RecyclerView;
 
 import edu.uw.tcss450.group8.chatapp.R;
 import edu.uw.tcss450.group8.chatapp.databinding.FragmentMessageListBinding;
+import edu.uw.tcss450.group8.chatapp.model.UserInfoViewModel;
 
 /**
- * Create an instance of Message List fragment.
- * Adapted from original code by Charles Bryan.
- *
- * @author Charles Bryan
- * @author JenHo Liao
- * @version 1.0
+ * A simple {@link Fragment} subclass.
  */
 public class MessageListFragment extends Fragment {
-    private MessageListViewModel mModel;
+
+    //The chat ID for "global" chat
+//    private static final int HARD_CODED_CHAT_ID = 1;
+
+    private MessageSendViewModel mSendModel;
+
+    private MessageListViewModel mChatModel;
+    private UserInfoViewModel mUserModel;
+
+    private int chatid;
+
+    public MessageListFragment() {
+        // Required empty public constructor
+    }
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mModel = new ViewModelProvider(getActivity()).get(MessageListViewModel.class);
+        ViewModelProvider provider = new ViewModelProvider(getActivity());
+        mUserModel = provider.get(UserInfoViewModel.class);
+        mChatModel = provider.get(MessageListViewModel.class);
+
+        mSendModel = provider.get(MessageSendViewModel.class);
+        chatid = MessageListFragmentArgs.fromBundle(getArguments()).getChatid();
     }
 
     @Override
@@ -41,15 +57,107 @@ public class MessageListFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        FragmentMessageListBinding binding = FragmentMessageListBinding.bind(getView());
-        mModel.addBlogListObserver(getViewLifecycleOwner(), blogList -> {
-            if (!blogList.isEmpty()) {
 
-                binding.listRoot.setAdapter(
-                        new MessageRecyclerViewAdapter(MessageGenerator.getMessageList())
-                );
-//                binding.layoutWait.setVisibility(View.GONE);
-            }
+        FragmentMessageListBinding binding = FragmentMessageListBinding.bind(getView());
+        mChatModel.getFirstMessages(chatid, mUserModel.getJwt());
+
+        //SetRefreshing shows the internal Swiper view progress bar. Show this until messages load
+        binding.swipeContainer.setRefreshing(true);
+
+        final RecyclerView rv = binding.recyclerMessages;
+        //Set the Adapter to hold a reference to the list FOR THIS chat ID that the ViewModel
+        //holds.
+        Log.e("chat id iew create in message frag", String.valueOf(chatid));
+        rv.setAdapter(new MessageRecyclerViewAdapter(
+                mChatModel.getMessageListByChatId(chatid),
+                mUserModel.getEmail()));
+
+
+        //When the user scrolls to the top of the RV, the swiper list will "refresh"
+        //The user is out of messages, go out to the service and get more
+        binding.swipeContainer.setOnRefreshListener(() -> {
+            mChatModel.getNextMessages(chatid, mUserModel.getJwt());
         });
+
+        mChatModel.addMessageObserver(chatid, getViewLifecycleOwner(),
+                list -> {
+                    /*
+                     * This solution needs work on the scroll position. As a group,
+                     * you will need to come up with some solution to manage the
+                     * recyclerview scroll position. You also should consider a
+                     * solution for when the keyboard is on the screen.
+                     */
+                    //inform the RV that the underlying list has (possibly) changed
+                    rv.getAdapter().notifyDataSetChanged();
+                    rv.scrollToPosition(rv.getAdapter().getItemCount() - 1);
+                    binding.swipeContainer.setRefreshing(false);
+                });
+
+        //Send button was clicked. Send the message via the SendViewModel
+        binding.buttonSend.setOnClickListener(button -> {
+            mSendModel.sendMessage(chatid,
+                    mUserModel.getJwt().toString(),
+                    binding.editMessage.getText().toString());
+        });
+//when we get the response back from the server, clear the edittext
+        mSendModel.addResponseObserver(getViewLifecycleOwner(), response ->
+                binding.editMessage.setText(""));
     }
 }
+
+
+
+
+//import android.os.Bundle;
+//
+//import androidx.annotation.NonNull;
+//import androidx.annotation.Nullable;
+//import androidx.fragment.app.Fragment;
+//import androidx.lifecycle.ViewModelProvider;
+//
+//import android.view.LayoutInflater;
+//import android.view.View;
+//import android.view.ViewGroup;
+//
+//import edu.uw.tcss450.group8.chatapp.R;
+//import edu.uw.tcss450.group8.chatapp.databinding.FragmentMessageListBinding;
+//
+///**
+// * Create an instance of Message List fragment.
+// * Adapted from original code by Charles Bryan.
+// *
+// * @author Charles Bryan
+// * @author JenHo Liao
+// * @version 1.0
+// */
+//public class MessageListFragment extends Fragment {
+//    private MessageListViewModel mModel;
+//
+//    @Override
+//    public void onCreate(@Nullable Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        mModel = new ViewModelProvider(getActivity()).get(MessageListViewModel.class);
+//    }
+//
+//    @Override
+//    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+//                             Bundle savedInstanceState) {
+//        // Inflate the layout for this fragment
+//        return inflater.inflate(R.layout.fragment_message_list, container, false);
+//    }
+//
+//    @Override
+//    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+//        super.onViewCreated(view, savedInstanceState);
+//        FragmentMessageListBinding binding = FragmentMessageListBinding.bind(getView());
+//        mModel.addBlogListObserver(getViewLifecycleOwner(), blogList -> {
+//            if (!blogList.isEmpty()) {
+//
+//                binding.listRoot.setAdapter(
+//                        new MessageRecyclerViewAdapter(MessageGenerator.getMessageList())
+//                );
+////                binding.layoutWait.setVisibility(View.GONE);
+//            }
+//        });
+//    }
+//}
--- app/src/main/java/edu/uw/tcss450/group8/chatapp/ui/comms/chat/MessageRecyclerViewAdapter.java
+++ app/src/main/java/edu/uw/tcss450/group8/chatapp/ui/comms/chat/MessageRecyclerViewAdapter.java
@@ -1,12 +1,18 @@
 
 package edu.uw.tcss450.group8.chatapp.ui.comms.chat;
 
+import android.content.res.Resources;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.card.MaterialCardView;
+import com.google.android.material.shape.CornerFamily;
 
 import java.util.List;
 
@@ -23,16 +29,12 @@ import edu.uw.tcss450.group8.chatapp.databinding.FragmentMessageCardBinding;
  */
 public class MessageRecyclerViewAdapter extends RecyclerView.Adapter<MessageRecyclerViewAdapter.MessageViewHolder> {
 
-    //Store all of the blogs to present
-    private final List<Message> mMessage;
+    private final List<Message> mMessages;
+    private final String mEmail;
 
-    /**
-     * Constructor for MessageRecyclerViewAdapter
-     *
-     * @param items list of message
-     */
-    public MessageRecyclerViewAdapter(List<Message> items) {
-        this.mMessage = items;
+    public MessageRecyclerViewAdapter(List<Message> messages, String email) {
+        this.mMessages = messages;
+        mEmail = email;
     }
 
     @NonNull
@@ -45,37 +47,106 @@ public class MessageRecyclerViewAdapter extends RecyclerView.Adapter<MessageRecy
 
     @Override
     public void onBindViewHolder(@NonNull MessageViewHolder holder, int position) {
-        holder.setMessage(mMessage.get(position));
+        holder.setMessage(mMessages.get(position));
     }
 
     @Override
     public int getItemCount() {
-        return this.mMessage.size();
+        return this.mMessages.size();
     }
 
     /**
      * Objects from this class represent an Individual row View from the List * of rows in the
      * Message Recycler View.
      */
-    public class MessageViewHolder extends RecyclerView.ViewHolder {
-        public final View mView;
-        public FragmentMessageCardBinding binding;
-        private Message mMessage;
+    class MessageViewHolder extends RecyclerView.ViewHolder {
+        private final View mView;
+        private FragmentMessageCardBinding binding;
 
-        public MessageViewHolder(View view) {
+        public MessageViewHolder(@NonNull View view) {
             super(view);
             mView = view;
             binding = FragmentMessageCardBinding.bind(view);
         }
 
-        /**
-         * Set message
-         *
-         * @param message message
-         */
         void setMessage(final Message message) {
-            mMessage = message;
-            binding.textMessageMessage.setText(message.getMessage());
+            final Resources res = mView.getContext().getResources();
+            final MaterialCardView card = binding.cardRoot;
+
+            int standard = (int) res.getDimension(R.dimen.chat_margin);
+            int extended = (int) res.getDimension(R.dimen.chat_margin_sided);
+
+            if (mEmail.equals(message.getSender())) {
+                //This message is from the user. Format it as such
+                binding.textMessage.setText(message.getMessage());
+                ViewGroup.MarginLayoutParams layoutParams =
+                        (ViewGroup.MarginLayoutParams) card.getLayoutParams();
+                //Set the left margin
+                layoutParams.setMargins(extended, standard, standard, standard);
+                // Set this View to the right (end) side
+                ((FrameLayout.LayoutParams) card.getLayoutParams()).gravity =
+                        Gravity.END;
+
+//                card.setCardBackgroundColor(
+//                        ColorUtils.setAlphaComponent(
+//                                res.getColor(R.color.primaryLightColor, null),
+//                                16));
+//                binding.textMessage.setTextColor(
+//                        res.getColor(R.color.secondaryTextColorFade, null));
+
+                card.setStrokeWidth(standard / 5);
+//                card.setStrokeColor(ColorUtils.setAlphaComponent(
+//                        res.getColor(R.color.primaryLightColor, null),
+//                        200));
+
+                //Round the corners on the left side
+                card.setShapeAppearanceModel(
+                        card.getShapeAppearanceModel()
+                                .toBuilder()
+                                .setTopLeftCorner(CornerFamily.ROUNDED,standard * 2)
+                                .setBottomLeftCorner(CornerFamily.ROUNDED,standard * 2)
+                                .setBottomRightCornerSize(0)
+                                .setTopRightCornerSize(0)
+                                .build());
+
+                card.requestLayout();
+            } else {
+                //This message is from another user. Format it as such
+                binding.textMessage.setText(message.getSender() +
+                        ": " + message.getMessage());
+                ViewGroup.MarginLayoutParams layoutParams =
+                        (ViewGroup.MarginLayoutParams) card.getLayoutParams();
+
+                //Set the right margin
+                layoutParams.setMargins(standard, standard, extended, standard);
+                // Set this View to the left (start) side
+                ((FrameLayout.LayoutParams) card.getLayoutParams()).gravity =
+                        Gravity.START;
+
+//                card.setCardBackgroundColor(
+//                        ColorUtils.setAlphaComponent(
+//                                res.getColor(R.color.secondaryLightColor, null),
+//                                16));
+
+                card.setStrokeWidth(standard / 5);
+//                card.setStrokeColor(ColorUtils.setAlphaComponent(
+//                        res.getColor(R.color.secondaryLightColor, null),
+//                        200));
+
+//                binding.textMessage.setTextColor(
+//                        res.getColor(R.color.secondaryTextColorFade, null));
+
+                //Round the corners on the right side
+                card.setShapeAppearanceModel(
+                        card.getShapeAppearanceModel()
+                                .toBuilder()
+                                .setTopRightCorner(CornerFamily.ROUNDED,standard * 2)
+                                .setBottomRightCorner(CornerFamily.ROUNDED,standard * 2)
+                                .setBottomLeftCornerSize(0)
+                                .setTopLeftCornerSize(0)
+                                .build());
+                card.requestLayout();
+            }
         }
     }
 }
